bbcad02e42092bef2a3f22e6e9a55f4aba4245f9
Letícia Borges <99838964+letiborges2510@users.noreply.github.com>
2025-02-20 20:41:35 -0300

    Add files via upload

---
+++ TAPWM/20.02.25/App.js
@@ -0,0 +1,19 @@
+const express = require ("express") 
+const app = express()
+
+app.listen(8081, function(){
+    console.log("Servidor Rodando")
+} )
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/html/index.html")
+})
+
+app.get("/produtos/:item/:quantidade", function(req, res){
+    res.send("Item: " +req.params.item + "<br>Quantidade: " +req.params.quantidade)
+})
+
+
+
+
+
